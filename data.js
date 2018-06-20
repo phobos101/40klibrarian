f5a50cbad6f0fe461e600a5d0dcd57a4b1c13a65
Rob Wilson <robwilson101@gmail.com>
2018-06-20 22:50:14 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = [
-    { title: 'The First Heretic', number: 1 },
-    { title: 'Xenos', number: 2 },
-    { title: 'Warrior of Ultramar', number: 3 },
-    { title: 'Fifteen Hours', number: 4 },
-    { title: 'Horus Rising', number: 5 },
+    { title: 'The First Heretic', number: 1, series: 'Horus Heresy', factions: ['Word Bearers', 'Ultramarines', 'Legio Custodes', 'Chaos'] },
+    { title: 'Xenos', number: 2, series: 'Eisenhorn', factions: ['Inquisition', 'Chaos'] },
+    { title: 'Warriors of Ultramar', number: 3, factions: ['Ultramarines', 'Mortifactors', 'Tyrannids', 'PDF'] },
+    { title: 'Fifteen Hours', number: 4, factions: ['PDF'] },
+    { title: 'Horus Rising', number: 5, series: 'Horus Heresy', factions: ['Luna Wolves', 'Sons of Horus', 'Interex', 'Megarachnid'] },
 ];
