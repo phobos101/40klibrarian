5fa0b927fb189352bdb6054ffa077d13100206fd
Rob Wilson <robwilson101@gmail.com>
2018-06-20 22:36:06 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = [
+    { title: 'The First Heretic', number: 1 },
+    { title: 'Xenos', number: 2 },
+    { title: 'Warrior of Ultramar', number: 3 },
+    { title: 'Fifteen Hours', number: 4 },
+    { title: 'Horus Rising', number: 5 },
+];
